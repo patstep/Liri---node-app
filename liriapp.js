683f8ae8b725d59214513ec7b568fca2c70e3356
patstep <patriciastepniak@gmail.com>
2018-04-16 13:46:30 -0400

    Adds Liri function

--- liriapp.js
+++ liriapp.js
@@ -1,12 +1,45 @@
 // ===== Loads required Node modules
 
-// Reads and sets environment variables
+// Reads and sets environment variables, requires keys
 require("dotenv").config();
+var keys = require('./keys.js');
+var fs = require('fs');
+var newCommand;
+var dataArr;
 
 var Twitter = require('twitter');
-var spotify = require('node-spofity-api');
+var client = new Twitter(keys.twitter);
+var params = {screen_name: 'instantclubhit'};
+
+var Spotify = require('node-spofity-api');
+var spotify = new Spotify(keys.spotify);
+var searchSong;
+
 var request = require('request');
-var fs = require('fs');
+var searchMovie;
 
-var keys = require("./keys"); // Imports API keys
 
+ // Liri function with required commands
+ function liri() {
+ 	if (command === 'my-tweets') {
+ 		myTweets();
+ 	} else if (command === 'spotify-this-sogn') {
+ 		spotifyThisSong();
+ 	} else if (command === 'movie-this') {
+ 		movieThis();
+ 	} else if (command === 'do-what-it-says') {
+ 		fs.readFile('random.txt', 'utf8', function (error, data) {
+ 			if (!error) {
+ 				newCommand = data;
+ 				console.log('Command received: ' + newCommand);
+ 				txtCommand();
+ 			}
+ 		});
+ 	} else {
+ 		console.log('Command not recognized.  Please try one of the following: my-tweets / spotify-this-song / movie-this / do-what-it-says')
+ 	}
+ }
+
+
+
+ 
